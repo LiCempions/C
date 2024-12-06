211097aaaeebc9d373bffc748a7a50fac0f9e333
LiCempions <nuvoletti.popo@gmail.com>
2024-12-06 19:32:10 +0100

    batch to use cl compiler

---
+++ compile.bat
@@ -0,0 +1,11 @@
+echo off
+set folder=%1
+set filename=%2
+if %filename%=="" (
+    set filename="*"
+)
+echo Will compile %folder%\%filename%.c
+cd .\%folder%
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" arm64
+cl "%filename%.c"
