04fc5937cf01e9cbafccb2a51882c36f38be4f2c
LiCempions <nuvoletti.popo@gmail.com>
2024-12-14 11:38:15 +0100

    compile to obj with optional folder. with guide

---
+++ objcompile.bat
@@ -0,0 +1,32 @@
+@echo off
+if [%*]==[] goto :tutorial
+
+set directory=%1
+if %directory:~0,1% neq \ set files=%* && goto :processFiles
+
+:folderFound
+cd %CD%%directory%
+@REM Strip folder name
+for /f "tokens=1*" %%i in ("%*") do ( set files=%%j )
+
+:processFiles
+echo Will compile to obj %files% in %CD%
+echo Initializing environment for compiler...
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" arm64 >nul
+
+echo Deleting existing object files...
+for %%f in ( %files% ) do del %%f.obj
+
+echo Compiling...
+for %%f in ( %files% ) do cl /c .\%%f.*
+goto :eof
+
+:tutorial
+echo:
+echo ---- OBJCOMPILE (mini) GUIDE ----
+echo:
+echo Syntax: objcompile.bat [\relativePath] file1 file2 ...
+echo:
+echo relativePath: (optional) path to the folder containing the files. Must contain a leading backslash. If omitted the current directory will be used.
+echo fileN: file names to compile to .obj files
+echo:
