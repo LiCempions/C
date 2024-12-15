0ce7b3186099666f5a48e51e3281c94636d73139
LiCempions <nuvoletti.popo@gmail.com>
2024-12-15 20:13:13 +0100

    small changes

--- compile.bat
+++ compile.bat
@@ -1,22 +1,29 @@
 echo off
 set folder=%1
 set filename=%2
-if %filename%=="" (
+if [%filename%]==[] (
     set filename="*"
 )
-if exist %folder%\%filename%.c (
-    echo Will compile %folder%\%filename%.c
+if exist %folder%\%filename%.* (
+    echo Will compile %folder%\%filename%
 ) else (
-    echo "%folder%\%filename%.c" not found
-    goto end
+    echo %folder%\%filename% not found
+    goto :end
 )
 
 cd .\%folder%
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" arm64
-cl "%filename%.c"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" arm64 >nul
+if exist "%filename%.exe" (
+    echo This will delete the old .exe file, rename it if you want to keep it
+    PAUSE
+    echo Deleting old executable...
+    del "%filename%.exe"
+)
+cl "%filename%.*"
 
 del %filename%.obj
 
 :end
-if %3==pause ( PAUSE )
+if [%2]==[pause] PAUSE
+if [%3]==[pause] PAUSE
