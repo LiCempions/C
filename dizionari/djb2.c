8759b5ca739382a3f02ac246203f7060ed8ae7a5
LiCempions <nuvoletti.popo@gmail.com>
2025-01-17 13:21:44 +0100

    Revert "Rossi messed up"

--- dizionari/djb2.c
+++ dizionari/djb2.c
@@ -1,13 +1,13 @@
-int h(const char* key){
-    int h = 5831, i=0;
+int h(char* key){
+    int h = 5831;
 
-    while (key[i] != '\0'){
+    while (*key != '\0'){
         // h = 33*h + *key;
         // h = 32*h + h + *key; Con questo formato potremmo utilizzare il formato seguente
-        h = (h<<5) + h + key[i];
+        h = (h<<5) + h + *key;
         // più veloce della normale moltiplicazione
         // in decimale: h<<5 è h * 2^5
-        i++;
+        key = key + sizeof(char);
     }
 
     return h;
