d2883f1b01ce3cb6b35fd256a73ac58ba2d52cd4
LiCempions <nuvoletti.popo@gmail.com>
2025-01-18 22:43:18 +0100

    can't inject in last position

--- lista-nodi/lista-nodi.c
+++ lista-nodi/lista-nodi.c
@@ -1,41 +1,33 @@
-// Assumption: keys are strings
 #include<stdlib.h>
 #include<stdio.h>
+#include"lista-nodi.h"
 
-typedef struct node {
-    float data;
-    node* next;
-} node;
-typedef node* nodeList;
-
-nodeList initList();
-
-nodeList push(nodeList);
-node* searchList(nodeList, float);
-
-void printNList(nodeList);
-
+/* ----- main ----- */
 int main(){
+    printf("Started\n");
+    node* node;
+    nodeList lista = initList();
 
+    lista = push(lista, 1.2);
+    lista = push(lista, 2.3);
+    lista = push(lista, 3.4);
+    lista = push(lista, 4.5);
+    lista = push(lista, 5.6);
+    lista = push(lista, 6.7);
+    lista = push(lista, 7.8);
+
+    printNList(lista);
+    node = getNode(lista, 30);
+    if (node!=NULL)
+        printf("%.2f\n", node->data);
+    printf("Terminato\n");
+    return 0;
 }
 
 nodeList initList(){
-    node* node = malloc(sizeof(node));
-    if (node == NULL)
-        return NULL;
-
-    (*node).data=0.0;
-    node->next=NULL;
-    return node;
+    return NULL;
 }
 
-nodeList push(nodeList list, float value){
-    node* node = malloc(sizeof(node));
-    node->data = value;
-    node->next = list;
-
-    return node;
-}
 node* searchList(nodeList L, float key){
     node* curr=L;
     while (curr!=NULL && curr->data!=key)
@@ -43,12 +35,90 @@ node* searchList(nodeList L, float key){
     
     return curr;
 }
+node* getNode(nodeList L, int index){
+    int i;
+    node* curr;
+
+    for (i=0, curr=L; curr!=NULL && i<index; i++, curr=curr->next){}
+
+    return curr;
+}
+
+nodeList push(nodeList list, float value){
+    node* newNode = malloc(sizeof(node));
+    newNode->data = value;
+    newNode->next = list;
+    newNode->prev = NULL;
+    if (list != NULL)
+        list->prev = newNode;
+
+    return newNode;
+    // Insert node after, copy first to second and override first: many more pointer overrides
+}
+/* problem: can't inject in the last position */
+nodeList inject(nodeList L, node* at, float value){
+    if (at==NULL)
+        return L;
+    
+    node* prevNode = (at==NULL) ? NULL : at->prev;
+    node* newNode = push(at, value);
+    newNode->prev = prevNode; // push is meant to insert at pos 0, so it set newNode->prev to NULL
+    if (prevNode==NULL){
+        return newNode; // if "at" was the first node, return newNode as the nodeList
+    } else {
+        prevNode->next = newNode;   // the previous node still points to "at"
+        return L;                   // the address of the first node did not change
+    }
+}
+nodeList injectThe(nodeList L, float key, float value){
+    node* at = searchList(L, key);
+    return inject(L, at, value);
+}
+nodeList injectAt(nodeList L, int index, float value){
+    node* at = getNode(L, index);
+    return inject(L, at, value);
+}
+
+nodeList chop(nodeList L){
+    if (L==NULL)
+        return NULL;
+
+    node* firstNode = L;
+    nodeList choppedL = firstNode->next;
+    if (choppedL!=NULL)
+        choppedL->prev = NULL;
+    free(firstNode);
+
+    return choppedL;
+}
+nodeList dropNode(nodeList L, node* node){
+    if (node == NULL)
+        return L;
+    
+    if (node->prev!=NULL)
+        node->prev = chop(node);
+    else
+        node = chop(node);
+    
+    return (node->prev==NULL) ? node : L;
+}
+nodeList dropThe(nodeList L, float key){
+    node* node = searchList(L, key);
+    return dropNode(L, node);
+}
+nodeList dropAt(nodeList L, int index){
+    node* node = getNode(L, index);
+    return dropNode(L, node);
+}
 
 void printNList(nodeList L){
     node* curr = L; // L è l'indirizzo del primo nodo
 
+    printf("\n==============\nValori lista a nodi:\t");
     do {
-        printf("Valore nodo: %f\n", curr->data);
+        printf("%.2f\t", curr->data);
         curr = curr->next;
     } while (curr!=NULL);
+
+    printf("\n");
 }
