af16e71b7476983bdfcee37d5757fee661cb6507
LiCempions <nuvoletti.popo@gmail.com>
2025-01-19 14:40:26 +0100

    proper demo

--- lista-nodi/LN-demo.c
+++ lista-nodi/LN-demo.c
@@ -12,11 +12,23 @@ int main(){
     lista = push(lista, 5.6);
     lista = push(lista, 6.7);
     lista = push(lista, 7.8);
+    lista = inject(lista, NULL, 0.1);
+    lista = injectAt(lista, 0, 8.9);
+    lista = injectThe(lista, 0.1, 0.5);
 
     printNList(lista);
-    node = getNode(lista, 30);
+
+    lista = chop(lista);
+    lista = dropThe(lista, 7.8);
+    lista = dropAt(lista, 1);
+
+    printNList(lista);
+
+    node = getNode(lista, -1);
     if (node!=NULL)
         printf("%.2f\n", node->data);
+    else
+        printf("Node was NULL\n");
     printf("Terminato\n");
     return 0;
 }
